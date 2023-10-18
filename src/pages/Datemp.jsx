dbb205234d6d24a1327788c47fe48ca39207c401
RadniDCV <rjdcv1@gmail.com>
2023-10-18 14:44:14 -0400

    cambio_4

--- src/pages/Datemp.jsx
+++ src/pages/Datemp.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import Table from "react-bootstrap/Table"
 
 function Datemp() {
+    const [employee , setEmployee] = useState([]);
+
+    useEffect(()=>{
+        axios
+        .get("https://hbackend.vercel.app/detemp")
+        .then((res)=>{
+            if(res.data.Status === "Success"){
+                setEmployee(res.data.Result)
+            } else{
+                console.log("Error", res.data)
+            }
+        })
+        .catch((err)=>console.log(err))
+    })
+
   return (
-    <div>Prueba</div>
+    <div>
+        <Table>
+            <thead>
+                <tr>
+                    <th>Codigo</th>
+                </tr>
+            </thead>
+            <tbody>
+                {employee.map((item, index)=>(
+                    <tr key={index}>
+                        <td>{item.empid}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    </div>
   )
 }
 
