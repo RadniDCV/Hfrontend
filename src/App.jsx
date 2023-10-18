42ee76dace1f4c059bc2d978d8f5bb52e9bf0d41
RadniDCV <rjdcv1@gmail.com>
2023-10-18 15:28:43 -0400

    cambi_6

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/employee" element={<Datemp/>}></Route>
+          <Route path="/empleados" element={<Datemp/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+    <hr/>
+    <Link to="/empleados">Empleados</Link>
+    </div>
   )
 }
 
