1e6d39317df958379cc2fa9662baf9b58a7e3249
RadniDCV <rjdcv1@gmail.com>
2023-10-18 23:05:41 -0400

    cam_11

--- src/pages/Datemp.jsx
+++ src/pages/Datemp.jsx
@@ -20,8 +20,20 @@ function Datemp() {
                 console.log(res.data.Result)
             }
         })
-        .catch((err)=>console.log(err, "Prueba")
-        
+        .catch((error) => {
+            if (error.response) {
+              // La solicitud fue hecha y el servidor respondió con un código de estado que no está en el rango 2xx
+              console.log("Respuesta del servidor:", error.response.data);
+              console.log("Código de estado HTTP:", error.response.status);
+              console.log("Cabeceras de respuesta:", error.response.headers);
+            } else if (error.request) {
+              // La solicitud fue hecha pero no se recibió respuesta
+              console.log("Solicitud enviada pero no se recibió respuesta");
+            } else {
+              // Ocurrió un error durante la configuración de la solicitud
+              console.log("Error de configuración de la solicitud:", error.message);
+            }
+        }
         )
     },[])
 
